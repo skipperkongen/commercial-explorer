aa49e5ed49c5690f3faa5d5bcc14d746512758fb
Pimin Konstantin Kefaloukos <skipperkongen@gmail.com>
2021-09-13 07:34:55 +0200

    bedre

--- commercial_explorer.py
+++ commercial_explorer.py
@@ -4,44 +4,68 @@ import streamlit as st
 import pydeck as pdk
 import random
 
+# Config
+
+st.set_page_config(layout="wide")
+
+# Description
+
 """
-# Shops, amenities & tourism.
+# Butikker, forlystelser & logi.
 > København
 """
+
+# Data
 
 df = pd.read_csv('commercial_kbh.csv').loc[:, [
     'lat', 'lon', 'kind', 'category'
 ]].dropna()
 
+# Sidebar
+
+st.sidebar.markdown('### Indstillinger')
+
 radius = st.sidebar.slider('Radius', value=50, min_value=10, max_value=200)
 
-ALL_LAYERS = {
-    cat: pdk.Layer(
+category = st.sidebar.selectbox(
+    'Vælg kategori',
+     df['category'].unique())
+
+all_kinds = list(sorted(df[df.category == category].kind.unique()))
+kinds = st.sidebar.multiselect(
+        "Vælg underkategorier", all_kinds, all_kinds
+    )
+
+## Render
+
+df_selected = df[(df.category==category) & (df.kind.isin(kinds))]
+
+st.write('Antal lokationer:', len(df_selected))
+
+selected_layers = [
+    pdk.Layer(
             "HexagonLayer",
-            data=df[df.category==cat],
+            data=df_selected,
             get_position=['lon', 'lat'],
             auto_highlight=True,
             radius=radius,#75,
             opacity=0.4,
             elevation_scale=1,
-            elevation_range=[1, 1000],
+            elevation_range=[5, 1000],
             extruded=True,
             coverage=1,
         )
-    for cat in df.category.unique()
-}
+]
 
-st.sidebar.markdown('### Map Layers')
-selected_layers = [
-    layer for layer_name, layer in ALL_LAYERS.items()
-    if st.sidebar.checkbox(layer_name, True)]
+mean_lat = df_selected.lat.mean()
+mean_lon = df_selected.lon.mean()
 
-if selected_layers:
+if len(df_selected):
     st.pydeck_chart(pdk.Deck(
         map_style="mapbox://styles/mapbox/light-v9",
-        initial_view_state={"latitude": 55.6836750,
-                            "longitude": 12.5730280, "zoom": 11, "pitch": 50},
+        initial_view_state={"latitude": mean_lat,
+                            "longitude": mean_lon, "zoom": 11, "pitch": 50},
         layers=selected_layers
     ))
 else:
-    st.error("Please choose at least one layer above.")
+    st.error("Vælg mere data for at vise kort")
